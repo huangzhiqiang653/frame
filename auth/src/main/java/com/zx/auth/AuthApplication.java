8888e91e1aae56e38112df70ef108d54e46b36cf
huamei09 <1278806998@qq.com>
2020-02-15 09:44:28 +0800

    去除 session redis

--- auth/src/main/java/com/zx/auth/AuthApplication.java
+++ auth/src/main/java/com/zx/auth/AuthApplication.java
@@ -4,10 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableRedisHttpSession
 @EnableTransactionManagement
 @SpringBootApplication
 @ComponentScan({
