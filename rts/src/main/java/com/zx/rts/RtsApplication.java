d3dc4416c6d468b2ec472d28c55692c04f171e7e
huangzhiqiang <490479710@qq.com>
2020-02-15 12:09:45 +0800

    1、系统增加旱厕改造模块，配置文件修改 2、旱厕改造启动类完善

--- rts/src/main/java/com/zx/rts/RtsApplication.java
+++ rts/src/main/java/com/zx/rts/RtsApplication.java
@@ -1,9 +1,20 @@
 package com.zx.rts;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
+@ComponentScan({
+        "com.zx.*.generator",
+        "com.zx.*.common",
+        "com.zx.*.controller",
+        "com.zx.*.service"
+})
+@MapperScan("com.zx.*.mapper")
 public class RtsApplication {
 
     public static void main(String[] args) {
