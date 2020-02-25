1e33915dd460621ab13815f1753520158a5137e4
shenyang <181553947@qq.com>
2020-02-25 10:04:11 +0800

    代码格式化

--- rts/src/main/java/com/zx/rts/dto/RtRecordRepairDto.java
+++ rts/src/main/java/com/zx/rts/dto/RtRecordRepairDto.java
@@ -1,17 +1,10 @@
 package com.zx.rts.dto;
 
 import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.zx.common.common.BaseEntityBean;
 import com.zx.rts.entity.RtRecordRepair;
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.experimental.Accessors;
-
-import java.time.LocalDateTime;
-import java.util.Date;
 
 /**
  * <p>
